98c0128a4f5217b0228ac5ebebcc2eb9a9cc421d
Nico Aricayos <nicolenevanz.aricayos@gmail.com>
2023-12-08 15:59:05 +0800

    can put an X and O in the board

---
+++ tictactoe.js
@@ -0,0 +1,13 @@
+const cells = document.querySelectorAll('.cell');
+let currentPlayer = 'X';
+
+cells.forEach(cell => {
+  cell.addEventListener('click', () => {
+    if (!cell.textContent) {
+      
+      console.log("Hello worldo")
+      cell.textContent = currentPlayer;
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+  });
+});
